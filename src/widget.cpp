2e8783a801a87f26c5300e0a309f2a0df7e8b080
mrbeanc <mrbeanc@qq.com>
2024-10-21 12:26:08 +0800

    feat: 窗口毛玻璃效果

--- src/widget.cpp
+++ src/widget.cpp
@@ -5,13 +5,18 @@
 #include <QFileInfo>
 #include <QWindow>
 #include <QKeyEvent>
+#include "utils/setWindowBlur.h"
+#include <QPainter>
+#include <QPen>
 
 Widget::Widget(QWidget *parent) :
         QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
     setWindowFlag(Qt::WindowStaysOnTopHint);
     setWindowFlag(Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TranslucentBackground);//设置窗口背景透明
     setWindowTitle("Windows Switcher");
+    setWindowBlur(hWnd()); // 设置窗口模糊, 必须配合Qt::WA_TranslucentBackground
 
     connect(qApp, &QApplication::focusWindowChanged, [this](QWindow *focusWindow) {
         if (focusWindow == nullptr) {
@@ -41,4 +46,13 @@ bool Widget::forceShow() {
     showMinimized();
     showNormal();
     return GetForegroundWindow() == (HWND)winId();
+}
+void Widget::paintEvent(QPaintEvent* event)//不绘制会导致鼠标穿透背景
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(Qt::NoPen);//取消边框//pen决定边框颜色
+    painter.setBrush(QColor(10, 10, 10, 50));
+    painter.drawRect(rect());
 }
