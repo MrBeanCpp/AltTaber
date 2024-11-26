eb81431edeec9b46ebb08f70fe55a7aac25caa4f
mrbeanc <mrbeanc@qq.com>
2024-11-26 16:32:37 +0800

    fix(uiautomation): impl operator=(move), avoid shadow copy of ptr, which makes lagging & crash

--- header/utils/uiautomation.h
+++ header/utils/uiautomation.h
@@ -4,6 +4,7 @@
 #include <UIAutomation.h>
 #include <QString>
 #include <QRect>
+#include <QDebug>
 
 class UIElement; // forward declaration
 /// simplify UIAutomation Library
@@ -28,6 +29,25 @@ public:
     UIElement() = default;
 
     explicit UIElement(IUIAutomationElement* pElement) : pElement(pElement) {}
+
+    UIElement(const UIElement&) = delete;
+    UIElement& operator=(const UIElement&) = delete; // 如果不实现又不delete，默认浅拷贝指针就寄了，直接卡死
+
+    // 最好用std::unique_ptr管理，避免自己实现移动
+    UIElement& operator=(UIElement&& other) noexcept {
+        // 1. 检查自移动
+        if (this != &other) {
+            // 2. 释放当前对象的资源
+            if (pElement) pElement->Release();
+
+            // 3. 移动资源
+            pElement = other.pElement;
+
+            // 4. 重置源对象
+            other.pElement = nullptr;
+        }
+        return *this;
+    }
 
     ~UIElement() {
         if (pElement) pElement->Release();
